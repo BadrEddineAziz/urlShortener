1007713d165426fb5d542303504c299c3885a01b
Badreddine Aziz <azizbadreddine63@gmail.com>
2024-01-18 21:03:33 +0100

    urlShortener

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="row">
+      <CreateLink />
+      <LinksList />
+    </div>
+  </template>
+  
+  <script setup>
+  import { defineProps, provide } from 'vue';
+  import CreateLink from '@/components/CreateLink.vue';
+  import LinksList from '@/components/LinksList.vue';
+  
+  const props = defineProps({
+    user_id: {
+      type: Number,
+      required: true
+    }
+  });
+  
+  provide('user_id', props.user_id);
+  </script>
+  
+  <style>
+  
+  </style>
---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UrlController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post("add/url",[UrlController::class, 'store']);
+Route::get("user/urls/{user_id}",[UrlController::class, 'index']);
+Route::delete("url/delete/{url_id}",[UrlController::class, 'destroy']);
+Route::put("urls/{shorten_url}",[UrlController::class, 'update']);
